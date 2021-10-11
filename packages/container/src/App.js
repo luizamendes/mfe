fa1a74e21fa97f8ac0c8e920fa2ce917273b479a
Luiza <luizaleitemendes@ibm.com>
2021-10-11 15:58:38 -0300

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { MarketingApp } from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
-    return <MarketingApp />
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    )
 }
